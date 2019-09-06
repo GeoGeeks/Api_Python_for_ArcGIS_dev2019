e7a9aa4736fb21834cd4e35ee89484e36bdb9ec2
dparradaza <daparrad@gmail.com>
2019-09-06 09:57:33 -0500

    cambios

--- ArcGis_Pandas.py
+++ ArcGis_Pandas.py
@@ -6,8 +6,9 @@ from arcgis import GIS
 import pandas as pd
 from arcgis.features import SpatialDataFrame
 import matplotlib as plt
+import numpy as np
 
-#conectar a cuenta arcgis online
+#conectar a cuenta arcgis online (No es obligatorio)
 gis=GIS("https://www.arcgis.com","dparra_geek","Esrico123*")
 
 
@@ -33,8 +34,32 @@ sdf3=pd.DataFrame(sdf.groupby(['LOCALIDAD'])['TOTAL'].sum())
 sdf3['TOTAL'].plot(kind='bar')
 
 #Agrupa por tipo de hurtos
-sdf4=pd.DataFrame(sdf.groupby(['CARACTERIZACION'])['TOTAL'].sum())
+sdf4=pd.DataFrame(sdf.groupby(['FECHA_HECHO'])['TOTAL'].sum())
 #Grafica de barras por tipo de hurto
-sdf4['TOTAL'].plot(kind='bar')
+sdf4['TOTAL'].plot(kind='line')
+
+##
+
+
+from bokeh.plotting import figure, output_file, show
+# create a new plot with a title and axis labels
+p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+y=np.random.randint(10, size=10)
+x=np.random.randint(15, size=10)
+
+
+output_file("lines.html")
+
+
+# add a line renderer with legend and line thickness
+p.line(y, x, legend="Temp.", line_width=2)
+
+# show the results
+show(p)
+
+
+
+
 
 
